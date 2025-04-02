f55f31010fa006a82ddd9e9150ff416bf8a8ffc5
maevefagan <fagan13@purdue.edu>
2025-04-02 15:43:46 -0400

    Add files via upload

---
+++ M1A_sub3_014_18_fagan13.m
@@ -0,0 +1,51 @@
+function[ ]= M1A_sub3_014_18_fagan13( )
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ENGR 132 
+% Program Description 
+% This MATLAB program will print a given statement based on the programmer,
+% myself, and 
+%
+% Function Call
+% replace this text with a comment that states the function call
+%
+% Input Arguments
+% replace this text with a commented list of the input arguments
+%
+% Output Arguments
+% replace this text with a commented list of the output arguments
+%
+% Assignment Information
+%   Assignment:     M1A, Problem 1
+%   Team member:    Maeve Fagan, fagan13@purdue.edu [repeat for each person]
+%   Team ID:        014-18
+%   Academic Integrity:
+%     [] We worked with one or more peers but our collaboration
+%        maintained academic integrity.
+%     Peers we worked with: Name, login@purdue [repeat for each]
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% ____________________
+%% INITIALIZATION
+
+
+%% ____________________
+%% CALCULATIONS
+
+
+%% ____________________
+%% FORMATTED TEXT/FIGURE DISPLAYS
+fprintf('Data successfully passed to subfunction [3] programmed by Maeve Fagan')
+
+%% ____________________
+%% RESULTS
+
+
+%% ____________________
+%% ACADEMIC INTEGRITY STATEMENT
+% We have not used source code obtained from any other unauthorized
+% source, either modified or unmodified. Neither have we provided
+% access to my code to another. The program we are submitting
+% is our own original work.
+
+
+
