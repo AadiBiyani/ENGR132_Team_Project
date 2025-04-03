e6ad1f11164a6d4ab3577d3946281f0706c8d58d
AadiBiyani <66646420+AadiBiyani@users.noreply.github.com>
2025-04-02 23:21:01 -0400

    Add files via upload
    Subfunction 1

---
+++ M1A_sub1_014_18_biyania.m
@@ -0,0 +1,50 @@
+function[output]= M1A_sub1_014_18_biyania(input)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ENGR 132 
+% Program Description 
+% This MATLAB program will print a given statement based on the programmer,
+% myself, and which subfunction passes.
+%
+% Function Call
+% [output]= M1A_sub1_014_18_biyania(input)
+%
+% Input Arguments
+% input is the functions input
+%
+% Output Arguments
+% outpu is the functions output
+%
+% Assignment Information
+%   Assignment:     M1A, Problem 1
+%   Team member:    Aadi Biyani, biyania@purdue.edu 
+%   Team ID:        014-18
+%   Academic Integrity:
+%     [] We worked with one or more peers but our collaboration
+%        maintained academic integrity.
+%     Peers we worked with: 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% ____________________
+%% INITIALIZATION
+
+
+%% ____________________
+%% CALCULATIONS
+
+
+%% ____________________
+%% FORMATTED TEXT/FIGURE DISPLAYS
+fprintf('Data successfully passed to subfunction [1] programmed by Aadi Biyani')
+
+%% ____________________
+%% RESULTS
+
+
+%% ____________________
+%% ACADEMIC INTEGRITY STATEMENT
+% We have not used source code obtained from any other unauthorized
+% source, either modified or unmodified. Neither have we provided
+% access to my code to another. The program we are submitting
+% is our own original work.
+
+end
