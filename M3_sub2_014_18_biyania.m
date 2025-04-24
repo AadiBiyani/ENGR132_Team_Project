8892d4bc02d98e4ce4f2ea8733b3c3996d31314d
AadiBiyani <66646420+AadiBiyani@users.noreply.github.com>
2025-04-24 00:17:09 -0400

    Update M3_sub2_014_18_biyania.m
    removed NaN values from dataset

--- M3_sub2_014_18_biyania.m
+++ M3_sub2_014_18_biyania.m
@@ -2,56 +2,60 @@ function [output] = M3_sub2_014_18_biyania(input)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % ENGR 132 
 % Program Description 
-% This program takes a dataset and applies a moving average filter 
-% to reduce noise in the data
+% This program takes a dataset, replaces NaNs using interpolation,
+% and applies a moving average filter to reduce noise in the data,
+% accounting for a time shift where time starts at 5.
 %
 % Function Call
 % [output] = M3_sub2_014_18_biyania(input)
 %
 % Input Arguments
-% input vector of numerical data
+% input - vector or matrix of numerical data (may include NaNs)
 %
 % Output Arguments
 % output - smoothed data after applying the moving average
 %
 % Assignment Information
-%   Assignment:     M02, Problem #2
+%   Assignment:     M03, Problem #2
 %   Team member:    Aadi Biyani, biyania@purdue.edu 
 %   Team ID:        014-18
-%   Academic Integrity:
-%     [] We worked with one or more peers but our collaboration
-%        maintained academic integrity.
-%     Peers we worked with: 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% initialize the window size and the number of rows and columns in the data
-% Create output variable filled with 0s so we can fill it with the result
 %% INITIALIZATION
-windowSize = 11;  % Changeable parameter
+windowSize = 11;  
 [numRows, numCols] = size(input);
 output = zeros(numRows, numCols); 
-
-%% ____________________
-%% CALCULATIONS
 halfWin = floor(windowSize / 2);
+timeVec = (0:numRows - 1)';
 
+interpInput = input;
+
+%% DATA PROCESSING
+% Interpolate to handle NaNs
+for col = 1:numCols
+    y = input(:, col);
+    validIdx = ~isnan(y);
+    
+    if sum(validIdx) >= 2
+        interpInput(:, col) = interp1(timeVec(validIdx), y(validIdx), timeVec, 'pchip', 'extrap');
+    elseif sum(validIdx) > 0
+        interpInput(:, col) = y(validIdx(1));
+    else
+        interpInput(:, col) = mean(input(~isnan(input)), 'all');
+    end
+end
+
+% Moving average filter
 for col = 1:numCols
     for i = 1:numRows
         startIdx = max(1, i - halfWin);
         endIdx = min(numRows, i + halfWin);
-        output(i, col) = mean(input(startIdx:endIdx, col));
+        output(i, col) = mean(interpInput(startIdx:endIdx, col));
     end
 end
 
+% Return the full smoothed dataset - filtering will be done in main file
 
-%% Not needed
-%% FORMATTED TEXT/FIGURE DISPLAYS
-
-%% No display needed
-%% RESULTS
-% Output is already computed and returned
-
-%% ____________________
 %% ACADEMIC INTEGRITY STATEMENT
 % We have not used source code obtained from any other unauthorized
 % source, either modified or unmodified. Neither have we provided
